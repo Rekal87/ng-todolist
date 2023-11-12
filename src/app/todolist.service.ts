dbd1cc6c0f64221f37a43e6ab26b0c20d2cb4a02
Nnamdi <nnamdi.b@gmail.com>
2023-11-12 05:35:09 +0100

    settablesignal

--- src/app/todolist.service.ts
+++ src/app/todolist.service.ts
@@ -1,10 +1,22 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, effect, signal, SettableSignal } from '@angular/core';
 import { DUMMY_TODOLIST_ARR } from './dummy-data';
+import { TodoListItem } from './component/todolistitem';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodolistService {
-  tlArray = signal(DUMMY_TODOLIST_ARR);
-  constructor() {}
+  tlArray: SettableSignal<TodoListItem[]>;
+  lsSaver = effect(() => {
+    localStorage.setItem('ng-todolist', JSON.stringify(this.tlArray()));
+  });
+  constructor() {
+    const itemsFromStorage = this.getItemsFromStorage();
+    this.tlArray = signal(itemsFromStorage);
+  }
+
+  getItemsFromStorage() {
+    const itemsStr = localStorage.getItem('ng-todolist');
+    return itemsStr ? JSON.parse(itemsStr) : DUMMY_TODOLIST_ARR;
+  }
 }
